8c6f11325501bc1405deebb1e2497e24349b6fdb
Konstantinos Soursos <kon.soursos_@hotmail.com>
2019-05-03 04:36:10 +0300

    Added the BestFirstSearch and the A* search algorithms. Also, visited nodes avoidance was added.

--- Maze.java
+++ Maze.java
@@ -1,13 +1,15 @@
 import java.util.Random;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 // The grid cosists of n x n placeholder Nodes, which contain the 4 possible paths
 // to the neighbouring Node. The paths are represented by the placeholder's edges.
 
 public class Maze {
-
     private LinkedList<Node> dataStructure;
+    private HashSet<String> visitedNodes;
 
     private Placeholder grid[][];
 
@@ -37,14 +39,105 @@ public class Maze {
 
         // Testing purposes
         // UCS(2000);
-        // BestFirstSearch();
-        // Astar();
+        // BestFirstSearch(2000);
+        // Astar(2000);
     }
 
     private void setParameters() {
         this.dataStructure = new LinkedList<>();
+        this.visitedNodes = new HashSet<>();
         this.createdNodesCounter = 0;
         this.visitedNodesCounter = 0;
+    }
+
+    private void Astar(int maxVisitedNods) {
+        this.setParameters();
+
+        this.dataStructure.add(new Node(this.startX, this.startY, this.grid, null, 0, 0, this));
+
+        while (!dataStructure.isEmpty()) {
+            if (this.visitedNodesCounter >= maxVisitedNods) {
+                System.out.println("\nVisited Nodes are over " + this.visitedNodesCounter
+                        + ".\nMax Nodes visited number exceeded.\nReturning..");
+                return;
+            }
+            this.visitedNodesCounter++;
+            // System.out.println("\n" + this.visitedNodesCounter);
+
+            Node minCostNode = null;
+            int minCost = Integer.MAX_VALUE;
+            for (int i = 0; i < this.dataStructure.size(); i++)
+
+                if (this.dataStructure.get(i).getCost()
+                        + this.dataStructure.get(i).manhattanDistance(targetX, targetY) < minCost) {
+                    minCost = this.dataStructure.get(i).getCost()
+                            + this.dataStructure.get(i).manhattanDistance(targetX, targetY);
+                    minCostNode = this.dataStructure.get(i);
+                }
+
+            Node node = minCostNode;
+            this.visitedNodes.add(node.getLocation());
+
+            // System.out.println("node " + node.getLocation() + " depth: " +
+            // node.getDepth() + " manhattan distance: "
+            // + node.manhattanDistance(this.targetX, this.targetY));
+            this.dataStructure.remove(node);
+
+            if (!node.checkIfTargetNode(this.targetX, this.targetY)) {
+                node.createChildren(this);
+
+                for (Node childNode : node.getChildren())
+                    if (!this.visitedNodes.contains(childNode.getLocation()))
+                        this.dataStructure.addLast(childNode);
+            } else {
+                this.printOutput(node, "A*");
+                return;
+            }
+        }
+    }
+
+    private void BestFirstSearch(int maxVisitedNods) {
+        this.setParameters();
+
+        this.dataStructure.add(new Node(this.startX, this.startY, this.grid, null, 0, 0, this));
+
+        while (!dataStructure.isEmpty()) {
+            if (this.visitedNodesCounter >= maxVisitedNods) {
+                System.out.println("\nVisited Nodes are over " + this.visitedNodesCounter
+                        + ".\nMax Nodes visited number exceeded.\nReturning..");
+                return;
+            }
+            this.visitedNodesCounter++;
+            // System.out.println("\n" + this.visitedNodesCounter);
+
+            Node minCostNode = null;
+            int minCost = Integer.MAX_VALUE;
+            for (int i = 0; i < this.dataStructure.size(); i++)
+
+                if (this.dataStructure.get(i).manhattanDistance(targetX, targetY) < minCost) {
+                    minCost = this.dataStructure.get(i).manhattanDistance(targetX, targetY);
+                    minCostNode = this.dataStructure.get(i);
+                }
+
+            Node node = minCostNode;
+            this.visitedNodes.add(node.getLocation());
+
+            // System.out.println("node " + node.getLocation() + " depth: " +
+            // node.getDepth() + " manhattan distance: " +
+            // node.manhattanDistance(this.targetX, this.targetY));
+            this.dataStructure.remove(node);
+
+            if (!node.checkIfTargetNode(this.targetX, this.targetY)) {
+                node.createChildren(this);
+
+                for (Node childNode : node.getChildren())
+                    if (!this.visitedNodes.contains(childNode.getLocation()))
+                        this.dataStructure.addLast(childNode);
+            } else {
+                this.printOutput(node, "BestFirstSearch");
+                return;
+            }
+        }
     }
 
     // Uncomment the lines below to see the exact path of the algorithm
@@ -54,8 +147,9 @@ public class Maze {
         this.dataStructure.add(new Node(this.startX, this.startY, this.grid, null, 0, 0, this));
 
         while (!dataStructure.isEmpty()) {
-            if(this.visitedNodesCounter >= maxVisitedNods) {
-                System.out.println("\nVisited Nodes are over " + this.visitedNodesCounter + ".\nMax Nodes visited number exceeded.\nReturning..");
+            if (this.visitedNodesCounter >= maxVisitedNods) {
+                System.out.println("\nVisited Nodes are over " + this.visitedNodesCounter
+                        + ".\nMax Nodes visited number exceeded.\nReturning..");
                 return;
             }
             this.visitedNodesCounter++;
@@ -71,22 +165,23 @@ public class Maze {
                 }
 
             Node node = minCostNode;
+            this.visitedNodes.add(node.getLocation());
 
             // System.out.println("node " + node.getLocation() + " depth: " +
             // node.getDepth());
             this.dataStructure.remove(node);
 
             if (!node.checkIfTargetNode(this.targetX, this.targetY)) {
-                node.createChildren();
+                node.createChildren(this);
 
                 for (Node childNode : node.getChildren())
-                    this.dataStructure.addLast(childNode);
+                    if (!this.visitedNodes.contains(childNode.getLocation()))
+                        this.dataStructure.addLast(childNode);
             } else {
                 this.printOutput(node, "UCS");
                 return;
             }
         }
-        System.out.println("problem");
     }
 
     private void printOutput(Node node, String algorithmName) {
@@ -110,7 +205,7 @@ public class Maze {
         for (int i = edgesToRemove; i > 0; i--) {
             int x = rand.nextInt(numberOfLines);
             int y = rand.nextInt(numberOfLines);
-            if(!grid[x][y].isIsolated())
+            if (!grid[x][y].isIsolated())
                 grid[x][y].removeEdge();
             else
                 i++;
@@ -128,6 +223,7 @@ public class Maze {
 
     private void printGrid(int numberOfLines) {
         String gridToShow[][] = new String[numberOfLines][numberOfLines];
+        System.out.println();
 
         for (int i = 0; i < numberOfLines; i++)
             for (int j = 0; j < numberOfLines; j++) {
@@ -139,8 +235,6 @@ public class Maze {
 
         for (int i = 0; i < numberOfLines; i++)
             System.out.println(Arrays.toString(gridToShow[i]));
-        ;
-
     }
 
     public boolean impossibleToFind() {
@@ -160,6 +254,12 @@ public class Maze {
             return true;
         }
 
+        if (this.startX == this.targetX && this.startY == this.targetY) {
+            System.out.println("\nStarting and Target nodes happen to be the same node.");
+            System.out.println("Reallocating..\n");
+            return true;
+        }
+
         return false;
     }
 
@@ -168,46 +268,49 @@ public class Maze {
     }
 
     public static void main(String[] args) {
-        // int numberOfLines;
-        // int percentageRemoving;
-        // int maxWeight;
+        int numberOfLines;
+        int percentageRemoving;
+        int maxWeight;
 
-        // Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        // System.out.println("Please insert the number of the grid's lines: ");
-        // numberOfLines = sc.nextInt();
+        System.out.println("Please insert the number of the grid's lines: ");
+        numberOfLines = sc.nextInt();
 
-        // System.out.println("Please insert the percentage of the grid's edges you want
-        // to remove(ex.80): ");
-        // percentageRemoving = sc.nextInt();
+        System.out.println("Please insert the percentage of the grid's edges you want to remove(ex.80): ");
+        percentageRemoving = sc.nextInt();
 
-        // System.out.println("Please insert the max weight of a grid's edge: ");
-        // percentageRemoving = sc.nextInt();
+        System.out.println("Please insert the max weight of a grid's edge: ");
+        maxWeight = sc.nextInt();
 
-        // Maze maze = new Maze(numberOfLines, percentageRemoving, maxWeight);
+        Maze maze = new Maze(numberOfLines, percentageRemoving, maxWeight);
 
         // Testing Purposes
-        Maze maze = new Maze(5, 20, 10);
+        // Maze maze = new Maze(5, 20, 10);
 
-        // while (true) {
-        // System.out.println("Please choose which algorithm you would like to run: \n1)
-        // UCS\n"
-        // + "2) Best-First Search\n3) A*\n4) Exit");
-        // int option = sc.nextInt();
+        System.out.println("\nPlease insert the maximum number of nodes that an algorithm is allowed to visit: ");
+        int maxNodes = sc.nextInt();
 
-        // if (option == 1)
-        // maze.UCS();
-        // else if (option == 2)
-        // maze.BestFirstSearch();
-        // else if (option == 3)
-        // maze.Astar();
-        // else if (option == 4) {
-        // System.out.println("Terminating the program.");
-        // sc.close();
-        // return;
-        // } else
-        // System.out.println("Wrong input.\nPlease try again.");
-        // }
+        // Testing Purposes
+        // int maxNodes = 2000;
+
+        while (true) {
+            System.out.println("Please choose which algorithm you would like to run: \n1) UCS\n2) Best-First Search\n3) A*\n4) Exit");
+            int option = sc.nextInt();
+
+            if (option == 1)
+                maze.UCS(maxNodes);
+            else if (option == 2)
+                maze.BestFirstSearch(maxNodes);
+            else if (option == 3)
+                maze.Astar(maxNodes);
+            else if (option == 4) {
+                System.out.println("Terminating the program.");
+                sc.close();
+                return;
+            } else
+                System.out.println("Wrong input.\nPlease try again.");
+        }
 
     }
 
